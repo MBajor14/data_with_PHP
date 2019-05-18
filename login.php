703e965e522e69ba150eefb98f2563dc149d7acf
Mike Bajor <michael.bajor@protonmail.com>
2019-05-18 10:16:10 -0400

    Update

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php
+    echo '<div class="page-minus-footer">';
     require('header.php');
 
     echo '
         <h1>Testing Login</h1>
     ';
 
+    echo '</div>';
     require('footer.php');
 ?>
