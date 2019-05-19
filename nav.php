581979fc64eb10e8c591fdd91cf72df6e316d69e
mbajor14 <michael.bajor@protonmail.com>
2019-05-19 19:14:21 -0400

    Update

--- nav.php
+++ nav.php
@@ -1,9 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="posts.php">PO$T!</a>
     <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a href="posts.php" class="nav-link">Add Post</a>
-        </li>
         <?php
             if(!$loggedIn){
                 echo '
@@ -15,7 +12,10 @@
             else{
                 echo '
                         <li class="nav-item">
-                            <a href="#" class="nav-link">Logout</a>
+                            <a href="posts.php" class="nav-link">Add Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="logout.php" class="nav-link">Logout</a>
                         </li>
                 ';
             }
