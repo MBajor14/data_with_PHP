ac4dc866b8449b0c4bbdbfc74fc60673e49cd8ec
mbajor14 <michael.bajor@protonmail.com>
2019-05-19 19:43:04 -0400

    Update

--- addPost.php
+++ addPost.php
@@ -1,7 +1,19 @@
 <?php
     require('header.php');
 
-
+    echo '
+            <form method="post" action="addPosts.php">
+              <div class="form-group">
+                <label for="title">Post Title</label>
+                <input type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Title..." name="title">
+              </div>
+              <div class="form-group">
+                <label for="body">Body</label>
+                <input type="text" class="form-control" id="body" placeholder="Body..." name="body">
+              </div>
+                <button name="addPost" type="submit" class="btn btn-primary">Add Post</button>
+            </form>
+    ';
 
     require('footer.php');
 ?>
