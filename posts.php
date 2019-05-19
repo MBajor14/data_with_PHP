1c33d9dc07cf16ccbe6bccd46af79a74d536154c
mbajor14 <michael.bajor@protonmail.com>
2019-05-19 13:20:09 -0400

    Update

--- posts.php
+++ posts.php
@@ -3,19 +3,26 @@
     require('header.php');
     require('../../safe/bajor_mysqli_connect.php');
 
+    if(isset($_GET['page'])){
+        $startPosition = ($_GET['page'] -1) * 5;
+        $page = $_GET['page'];
+    }
+    else{
+        $startPosition = 0;
+        $page = 1;
+    }
+
     $query = "
         SELECT u.name, p.date_time, p.title, p.body 
         FROM users AS u, posts AS p 
         WHERE u.user_id=p.user_id
-        LIMIT 5;
+        LIMIT ?,5;
     ";
 
     $stmt = $dbc -> prepare($query);
-
+    $stmt -> bind_param($startPosition);
     $stmt -> execute();
-
     $stmt -> store_result();
-
     $stmt -> bind_result($name,$date,$title,$body);
 
 
@@ -35,14 +42,21 @@
         };
     echo '</ul>';
 
-    echo '
-            <div class="page-jump">
+    $nextPage = $page + 1;
+    $previousPage = $page - 1;
+
+    echo '<div class="page-jump">';
+    if($page > 1){
+        echo '
                 <div>
                     <i class="fas fa-long-arrow-alt-left"></i>
-                    <a href="">Last 5</a>
+                    <a href="posts.php?page=$nextPage">Last 5</a>
                 </div>
+        ';
+    }
+    echo '
                 <div>
-                    <a href="">Next 5</a>
+                    <a href="posts.php?page=$previousPage">Next 5</a>
                     <i class="fas fa-long-arrow-alt-right"></i>
                 </div>
             </div>
