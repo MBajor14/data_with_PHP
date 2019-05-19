9f74467dcf35dcf895abb14445e49ccf6f99df24
mbajor14 <michael.bajor@protonmail.com>
2019-05-19 19:49:21 -0400

    Update

--- addPost.php
+++ addPost.php
@@ -1,19 +1,36 @@
 <?php
     require('header.php');
 
-    echo '
-            <form method="post" action="addPosts.php">
-              <div class="form-group">
-                <label for="title">Post Title</label>
-                <input type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Title..." name="title">
-              </div>
-              <div class="form-group">
-                <label for="body">Body</label>
-                <input type="text" class="form-control" id="body" placeholder="Body..." name="body">
-              </div>
-                <button name="addPost" type="submit" class="btn btn-primary">Add Post</button>
-            </form>
-    ';
+    if(isset($_POST['addPost'])){
+        if(isset($_POST['title']) && isset($_POST['body'])){
+            $title = $_POST['title'];
+            $body = $_POST['body'];
+        }
 
+        if(empty($title) || empty($body))    {
+            $error = 'Please fill out all the fields to add post';
+            header("Location: login.php?error=".$error);
+            exit();
+        }
+    }
+
+    if(isset($_GET['error'])){
+        $error = $_GET['error'];
+        echo "<div class='alert alert-warning msg' role='alert'>$error</div>";
+    }
+?>
+    <form method="post" action="addPosts.php">
+      <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Title..." name="title">
+      </div>
+      <div class="form-group">
+        <label for="body">Body</label>
+        <input type="text" class="form-control" id="body" placeholder="Body..." name="body">
+      </div>
+        <button name="addPost" type="submit" class="btn btn-primary">Add Post</button>
+    </form>
+
+<?php
     require('footer.php');
 ?>
